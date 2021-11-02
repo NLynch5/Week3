4e3ab72683b9e1ea738bd8ce5240cf699262dccf
John Doe <natalieclairelynch@gmail.com>
2021-11-02 11:41:47 +0000

    update to keycode challenge

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-const 
+const eventKey = document.getElementsByClassName("eventKey");
+const eventCode = document.getElementsByClassName("eventCode");
+const eventWhich = document.getElementsByClassName("eventWhich");
+
+
+document.addEventListener("click", (event) =>{
+eventKey=event.
+})
